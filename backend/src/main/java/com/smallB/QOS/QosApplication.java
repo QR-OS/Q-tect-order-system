b426342594ee7c13a0f53af59b0bda3e1aaa33d6
small-j <44703262+small-j@users.noreply.github.com>
2020-08-09 17:20:35 +0900

    Init Backend

---
+++ backend/src/main/java/com/smallB/QOS/QosApplication.java
@@ -0,0 +1,13 @@
+package com.smallB.QOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QosApplication.class, args);
+	}
+
+}
