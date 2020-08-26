5ba8f5656db566a0d4ffbe3bf40c240e911c3085
김지윤 <smallj1044@gmail.com>
2020-08-27 01:43:46 +0900

    Edit UserDTO

--- backend/src/main/java/com/smallB/QOS/dto/UserDTO.java
+++ backend/src/main/java/com/smallB/QOS/dto/UserDTO.java
@@ -1,4 +1,18 @@
 package com.smallB.QOS.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class UserDTO {
+    private String user_id;
+    private String user_name;
+    private String user_pw;
+    private String user_ph;
+    private String user_email;
+    private String store_id;
+    private int status;
 }
