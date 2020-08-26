8d5056b0ca9cfc3035ff7caf71294f5b3aa4d693
김지윤 <smallj1044@gmail.com>
2020-08-27 01:17:19 +0900

    connect to DB

--- backend/src/main/java/com/smallB/QOS/QosApplication.java
+++ backend/src/main/java/com/smallB/QOS/QosApplication.java
@@ -1,7 +1,14 @@
 package com.smallB.QOS;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class QosApplication {
@@ -10,4 +17,14 @@ public class QosApplication {
 		SpringApplication.run(QosApplication.class, args);
 	}
 
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+		sessionFactory.setMapperLocations(res);
+
+		return sessionFactory.getObject();
+	}
 }
