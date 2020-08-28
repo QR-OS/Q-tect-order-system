acf8cd7cdcb00bb35b82492a50244ca3faeb877d
김지윤 <smallj1044@gmail.com>
2020-08-28 21:37:23 +0900

    Apply builder in UserServiceImpl

--- backend/src/main/java/com/smallB/QOS/service/UserServiceImpl.java
+++ backend/src/main/java/com/smallB/QOS/service/UserServiceImpl.java
@@ -3,6 +3,8 @@ package com.smallB.QOS.service;
 import com.smallB.QOS.dao.UserDao;
 import com.smallB.QOS.domain.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +15,16 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public boolean register(UserDto userDto) throws Exception {
-        userDao.register(userDto);
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(userDto.getUser_pw());
+        UserDto newUser = UserDto.builder()
+                .user_id(userDto.getUser_id())
+                .user_name(userDto.getUser_name())
+                .user_email(userDto.getUser_email())
+                .user_ph(userDto.getUser_ph())
+                .user_pw(encodedPassword)
+                .build();
+        userDao.register(newUser);
         return true;
     }
 
