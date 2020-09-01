3c82ee7622b98ff176419cdbbf38b57ed8a71d54
김지윤 <smallj1044@gmail.com>
2020-09-01 20:42:10 +0900

    Add Vaildation

--- backend/src/main/java/com/smallB/QOS/user/domain/SessionRequestDto.java
+++ backend/src/main/java/com/smallB/QOS/user/domain/SessionRequestDto.java
@@ -2,10 +2,15 @@ package com.smallB.QOS.user.domain;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
+
+import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
 public class SessionRequestDto {
+    @NotNull(message = "user_id must not null")
     private String user_id;
+    @NotNull(message = "user_pw must not null")
     private String user_pw;
 }
--- backend/src/main/java/com/smallB/QOS/user/domain/UserDto.java
+++ backend/src/main/java/com/smallB/QOS/user/domain/UserDto.java
@@ -1,20 +1,23 @@
 package com.smallB.QOS.user.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+
+import javax.validation.Valid;
 
 @AllArgsConstructor
 @Getter
 @Setter
 @Builder
 public class UserDto {
+    @Valid
     private String user_id;
+    @Valid
     private String user_name;
+    @Valid
     private String user_pw;
     private String user_ph;
     private String user_email;
     private String store_id = null;
+    @Valid
     private int status = 1;
 }
