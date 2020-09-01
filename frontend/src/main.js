2a188b839d5ec07a0e0a99d794f1f423193c8246
김지윤 <smallj1044@gmail.com>
2020-09-01 11:54:21 +0900

    Edit front setting

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
+import axios from 'axios';
 
 Vue.config.productionTip = false;
-
+axios.defaults.baseURL = '/api'
+axios.defaults.headers.common['Cache-Control'] = 'no-cache'
 new Vue({
   router,
   store,
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../components/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -18,6 +19,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login
   }
 ];
 
