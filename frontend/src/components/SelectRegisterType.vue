230d1a18bb6024938cb2d1364d688fef0f111cc7
EunJi Heo <gjdms611@gmail.com>
2020-09-02 14:27:11 +0900

    create SelectRegisterType Page

---
+++ frontend/src/components/SelectRegisterType.vue
@@ -0,0 +1,40 @@
+<!-- SelectRegisterType -->
+
+<template>
+  <v-container class="fill-height" fluid>
+    <v-row>
+      <v-col>
+        <h1>회원가입</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-btn outlined @click="MoveToOwnerRegister">
+        매장 관리자
+      </v-btn>
+      <v-btn outlined @click="MoveToConsumerRegister">
+        일반회원
+      </v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  methods: {
+    MoveToOwnerRegister() {
+      this.$router.push({
+        name: "Register",
+        params: { is_owner: 1 },
+      });
+    },
+    MoveToConsumerRegister() {
+      this.$router.push({
+        name: "Register",
+        params: { is_owner: 0 },
+      });
+    },
+  },
+};
+</script>
+
+<style></style>
