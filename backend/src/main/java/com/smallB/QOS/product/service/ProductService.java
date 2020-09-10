41b449b2d7fb99f285459680d7c3da2add056822
김지윤 <smallj1044@gmail.com>
2020-09-10 17:20:13 +0900

    Add ProductService - register - getUserById - authenticate - updateUser

---
+++ backend/src/main/java/com/smallB/QOS/product/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.smallB.QOS.product.service;
+
+import com.smallB.QOS.product.domain.ProductDto;
+import java.util.List;
+
+public interface ProductService {
+
+    ProductDto getProduct(String product_id, String store_id) throws Exception;
+
+    List<ProductDto> getProducts(String store_id) throws Exception;
+
+    ProductDto createProduct(ProductDto productDto) throws Exception;
+
+    boolean updateProduct(ProductDto productDto) throws  Exception;
+}
