2824f3a539ce60e108845a352271e8d983bc9443
김지윤 <smallj1044@gmail.com>
2020-09-10 17:20:35 +0900

    Add ProductDao

---
+++ backend/src/main/java/com/smallB/QOS/product/dao/ProductDao.java
@@ -0,0 +1,19 @@
+package com.smallB.QOS.product.dao;
+
+import com.smallB.QOS.product.domain.ProductDto;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductDao {
+    ProductDto findProductByProductIdAndStoreId(String product_id, String store_id) throws  Exception;
+
+    List<ProductDto> findProductByStoreId(String store_id) throws Exception;
+
+    void createProduct(ProductDto productDto) throws Exception;
+
+    ProductDto productIdDuplicationCheck(String product_id) throws Exception;
+
+    boolean updateProductByProductIdAndStoreId(ProductDto productDto) throws Exception;
+}
