c2d71f1d43de110877a2c08f7d62dc74337707bb
김지윤 <smallj1044@gmail.com>
2020-10-25 21:47:30 +0900

    Update OrderServiceImpl.java
    Add createOrder in OrderServiceImpl

--- backend/src/main/java/com/smallB/QOS/order/service/OrderServiceImpl.java
+++ backend/src/main/java/com/smallB/QOS/order/service/OrderServiceImpl.java
@@ -2,9 +2,12 @@ package com.smallB.QOS.order.service;
 
 import com.smallB.QOS.order.dao.OrderDao;
 import com.smallB.QOS.order.domain.OrderDto;
+import com.smallB.QOS.order.error.OrderCreateFailException;
 import com.smallB.QOS.order.error.OrderNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 import static java.util.Objects.isNull;
 
@@ -19,6 +22,17 @@ public class OrderServiceImpl implements OrderService{
         OrderDto orderDto = orderDao.findOrder(order_id,store_id);
         if(isNull(orderDto)){
             throw new OrderNotFoundException(order_id);
+        }
+        return orderDto;
+    }
+
+    @Override
+    public OrderDto createOrder(OrderDto orderDto) throws Exception{
+        orderDto.setOrder_time(new Date());
+
+        Boolean result = orderDao.createOrder(orderDto);
+        if(result != true) {
+            throw new OrderCreateFailException();
         }
         return orderDto;
     }
