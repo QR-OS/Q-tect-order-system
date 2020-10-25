70deba6097bcd12cfeff5e769dde92fed991f419
김지윤 <smallj1044@gmail.com>
2020-10-25 21:46:36 +0900

    Update OrderDao.java
    Add POST /api/order

--- backend/src/main/java/com/smallB/QOS/order/dao/OrderDao.java
+++ backend/src/main/java/com/smallB/QOS/order/dao/OrderDao.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface OrderDao {
     OrderDto findOrder(String order_id, String store_id) throws Exception;
+
+    boolean createOrder(OrderDto orderDto) throws Exception;
+
 }
