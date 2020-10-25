c1cd679f953d21c30357d9fc433e005f22d04ec2
김지윤 <smallj1044@gmail.com>
2020-10-25 17:49:59 +0900

    Create OrderDto.java

---
+++ backend/src/main/java/com/smallB/QOS/order/domain/OrderDto.java
@@ -0,0 +1,32 @@
+package com.smallB.QOS.order.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class OrderDto {
+    private boolean order_type;
+    private Date order_time;
+    private Date book_time;
+    @NotEmpty @NotBlank @NotNull
+    private String order_id;
+    @PositiveOrZero
+    private int total_price;
+    private String pay_type;
+    private String order_state;
+    @NotEmpty @NotBlank @NotNull
+    private String store_id;
+    @NotEmpty @NotBlank @NotNull
+    private String user_id;
+}
