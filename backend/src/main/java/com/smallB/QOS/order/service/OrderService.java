e7f8fe742f50c7468213e97261f8252e47b902c0
김지윤 <smallj1044@gmail.com>
2020-10-25 22:04:51 +0900

    Update OrderService.java
    Add GET /orders/:store_id

--- backend/src/main/java/com/smallB/QOS/order/service/OrderService.java
+++ backend/src/main/java/com/smallB/QOS/order/service/OrderService.java
@@ -2,11 +2,11 @@ package com.smallB.QOS.order.service;
 
 import com.smallB.QOS.order.domain.OrderDto;
 import org.springframework.stereotype.Service;
-
+import java.util.List;
 
 @Service
 public interface OrderService {
     OrderDto getOrder(String order_id, String store_id) throws Exception;
-
+    List<OrderDto> getOrders(String store_id) throws Exception;
     OrderDto createOrder(OrderDto orderDto) throws Exception;
 }
