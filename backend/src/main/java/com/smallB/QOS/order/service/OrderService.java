c1db127a52d5c7b7ae9bddbdb3fc798a84a5c1fa
김지윤 <smallj1044@gmail.com>
2020-10-25 21:47:17 +0900

    Update OrderService.java
    Add createOrder in OrderService

--- backend/src/main/java/com/smallB/QOS/order/service/OrderService.java
+++ backend/src/main/java/com/smallB/QOS/order/service/OrderService.java
@@ -1,7 +1,12 @@
 package com.smallB.QOS.order.service;
 
 import com.smallB.QOS.order.domain.OrderDto;
+import org.springframework.stereotype.Service;
 
+
+@Service
 public interface OrderService {
     OrderDto getOrder(String order_id, String store_id) throws Exception;
+
+    OrderDto createOrder(OrderDto orderDto) throws Exception;
 }
