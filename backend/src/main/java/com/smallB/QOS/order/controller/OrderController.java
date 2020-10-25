048a92d69e8e9cd429c061631d12b1fe1f8dde3d
김지윤 <smallj1044@gmail.com>
2020-10-25 21:46:32 +0900

    Update OrderController.java
    Add POST /api/order

--- backend/src/main/java/com/smallB/QOS/order/controller/OrderController.java
+++ backend/src/main/java/com/smallB/QOS/order/controller/OrderController.java
@@ -5,15 +5,13 @@ import com.smallB.QOS.order.service.OrderService;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @EnableAutoConfiguration
-@MapperScan(basePackages = "com.smallB.QOS.product.dao")
+@MapperScan(basePackages = "com.smallB.QOS.Order.dao")
 public class OrderController {
 
     @Autowired
@@ -23,6 +21,9 @@ public class OrderController {
     public OrderDto getOrder(@PathVariable @Valid String order_id, @PathVariable @Valid String store_id) throws Exception{
         return orderService.getOrder(order_id,store_id);
     }
-    
 
+    @PostMapping("/order")
+    public OrderDto createOrder(@RequestBody @Valid OrderDto orderDto) throws Exception{
+        return orderService.createOrder(orderDto);
+    }
 }
