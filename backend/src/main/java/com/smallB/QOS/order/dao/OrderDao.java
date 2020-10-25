db73d0351823873206883266699456933211075a
김지윤 <smallj1044@gmail.com>
2020-10-25 17:49:52 +0900

    Create OrderDao.java
    Add  findOrder function in OrderDao

---
+++ backend/src/main/java/com/smallB/QOS/order/dao/OrderDao.java
@@ -0,0 +1,9 @@
+package com.smallB.QOS.order.dao;
+
+import com.smallB.QOS.order.domain.OrderDto;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderDao {
+    OrderDto findOrder(String order_id, String store_id) throws Exception;
+}
