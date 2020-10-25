13f243b776d890b96edf8c4c39572389e2b31029
김지윤 <smallj1044@gmail.com>
2020-10-25 22:04:48 +0900

    Update OrderDao.java
    Add GET /orders/:store_id

--- backend/src/main/java/com/smallB/QOS/order/dao/OrderDao.java
+++ backend/src/main/java/com/smallB/QOS/order/dao/OrderDao.java
@@ -2,11 +2,12 @@ package com.smallB.QOS.order.dao;
 
 import com.smallB.QOS.order.domain.OrderDto;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface OrderDao {
     OrderDto findOrder(String order_id, String store_id) throws Exception;
-
+    List<OrderDto> findOrders(String store_id) throws Exception;
     boolean createOrder(OrderDto orderDto) throws Exception;
 
 }
