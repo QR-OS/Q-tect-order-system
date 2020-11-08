b028b0d81722512333ca11d784d176caae511880
김지윤 <smallj1044@gmail.com>
2020-11-08 15:59:15 +0900

    Update DetailOrder Domain
    Add property `product_name`, `product_price`

--- backend/src/main/java/com/smallB/QOS/detailOrder/domain/DetailOrderDto.java
+++ backend/src/main/java/com/smallB/QOS/detailOrder/domain/DetailOrderDto.java
@@ -3,16 +3,21 @@ package com.smallB.QOS.detailOrder.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Service;
+import lombok.Setter;
+
+import javax.validation.constraints.Positive;
 
 @Getter
-@Service
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class DetailOrderDto {
+    @Positive
     private int order_id;
     private String store_id;
     private String product_id;
     private int order_quantity;
     private String user_id;
+    private String product_name;
+    private int product_price;
 }
