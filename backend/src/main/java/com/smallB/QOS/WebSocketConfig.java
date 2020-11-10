da6b34dd31060a40ca3a7bc9ce72a18859fe06f2
Hwangsoomin <maum97@naver.com>
2020-11-10 19:14:17 +0900

    start socket

---
+++ backend/src/main/java/com/smallB/QOS/WebSocketConfig.java
@@ -0,0 +1,10 @@
+package com.smallB.QOS;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+}
