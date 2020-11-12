b960fb94870cff2f50692c01e536e20ef7398ac3
EunJi Heo <gjdms611@gmail.com>
2020-11-12 21:38:38 +0900

    Fix StoreMain Design
    - StoreMain 페이지의 디자인 문제를 수정했습니다.

--- frontend/src/views/store/StoreMain.vue
+++ frontend/src/views/store/StoreMain.vue
@@ -28,12 +28,11 @@
         >
           <template v-slot:placeholder>
             <v-row class="fill-height ma-0" align="center" justify="center">
-              <v-progress-circular
-                indeterminate
-                color="grey lighten-5"
-              ></v-progress-circular>
-            </v-row> </template
-        ></v-img>
+              <v-progress-circular indeterminate color="grey lighten-5">
+              </v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
       </v-col>
       <v-col cols="7" class="mt-10">
         <v-row>
@@ -49,128 +48,180 @@
           open {{ storeInfo.open_time }} ~ close
           {{ storeInfo.close_time }}
         </v-row>
-        <v-row class="ml-3">
-          <v-subheader :inset="inset">
-            휴무일
-          </v-subheader>
+        <v-row class="mx-3">
+          <v-col md="3" sm="5">
+            <v-subheader :inset="inset">
+              휴무일
+            </v-subheader>
+          </v-col>
+          <v-col
+            class="mx-0 px-0"
+            md="2"
+            sm="2"
+            v-for="(item, idx) in storeInfo.holiday"
+            :key="idx"
+          >
+            <v-chip>{{ item }}</v-chip>
+          </v-col>
         </v-row>
       </v-col>
     </v-row>
     <v-row>
-      <!--자세한 메장정보-->
-      <v-expansion-panels flat>
-        <v-expansion-panel>
-          <v-expansion-panel-header> </v-expansion-panel-header>
-          <v-expansion-panel-content>
-            <v-col>
+      <v-col>
+        <!--자세한 메장정보-->
+        <v-expansion-panels flat>
+          <v-expansion-panel>
+            <v-expansion-panel-header />
+            <v-expansion-panel-content>
               <v-row>
-                <v-subheader :inset="inset">
-                  사업자 번호
-                </v-subheader>
+                <v-col>
+                  <v-row>
+                    <v-col>
+                      <v-subheader :inset="inset">
+                        사업자 번호
+                      </v-subheader>
+                      <v-divider></v-divider>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col class="ml-4">{{ storeInfo.store_num }} </v-col>
+                  </v-row>
+                </v-col>
+
+                <v-col>
+                  <v-row>
+                    <v-col>
+                      <v-subheader :inset="inset">
+                        매장 전화번호
+                      </v-subheader>
+                      <v-divider></v-divider>
+                    </v-col>
+                  </v-row>
+                  <v-row>
+                    <v-col class="ml-4">{{ storeInfo.store_tel }} </v-col>
+                  </v-row>
+                </v-col>
+                <v-col>
+                  <v-row>
+                    <v-col>
+                      <v-subheader :inset="inset">
+                        매장 분류
+                      </v-subheader>
+                      <v-divider></v-divider>
+                    </v-col>
+                  </v-row>
+                  <v-row class="mx-4">
+                    <v-col
+                      class="mx-0 px-0"
+                      align="center"
+                      v-for="(item, idx) in storeCategory"
+                      :key="idx"
+                    >
+                      <v-chip>{{ item.store_type }}</v-chip>
+                    </v-col>
+                  </v-row>
+                </v-col>
               </v-row>
               <v-row>
-                <v-divider class="mr-200"></v-divider>
+                <v-col>
+                  <v-row>
+                    <v-col>
+                      <v-subheader :inset="inset">
+                        가게위치
+                      </v-subheader>
+                      <v-divider></v-divider>
+                    </v-col>
+                  </v-row>
+                  <v-row class="ml-4">
+                    <v-col md="2" sm="3" class="grey--text">
+                      우편번호
+                    </v-col>
+                    <v-col>
+                      {{ storeInfo.post_num }}
+                    </v-col>
+                  </v-row>
+                  <v-row class="ml-4">
+                    <v-col md="2" sm="3" class="grey--text">주소</v-col>
+                    <v-col>{{ storeInfo.address1 }}</v-col>
+                  </v-row>
+                  <v-row class="ml-4">
+                    <v-col md="2" sm="3" class="grey--text">
+                      상세주소
+                    </v-col>
+                    <v-col>
+                      {{ storeInfo.address2 }}
+                    </v-col>
+                  </v-row>
+                </v-col>
               </v-row>
-              <v-row>{{ storeInfo.store_num }}</v-row>
-              <v-row>
-                <v-subheader :inset="inset">
-                  매장 전화번호
-                </v-subheader>
-                <v-divider light></v-divider>
-                {{ storeInfo.store_tel }}
-              </v-row>
-              <v-row>
-                <v-subheader :inset="inset">
-                  매장 분류
-                </v-subheader>
-                <v-divider light></v-divider>
-                <v-chip>한식</v-chip><v-chip>분식</v-chip>
-              </v-row>
-            </v-col>
-            <v-col>
-              <v-row>
-                <v-subheader :inset="inset">
-                  가게위치
-                </v-subheader>
-              </v-row>
-              <v-row><v-divider light></v-divider></v-row>
-              <div class="" color="gray">우편번호</div>
-              {{ storeInfo.post_num }}
-              <div small color="gray">주소</div>
-              {{ storeInfo.address1 }}
-              <div small color="gray">상세주소</div>
-              {{ storeInfo.address2 }}
-            </v-col>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-col>
     </v-row>
     <v-row>
-      <v-divider light></v-divider>
+      <v-divider></v-divider>
     </v-row>
     <v-row class="mt-5 mr-5 mb-5">
       <span class="text-h4">메뉴</span>
       <v-spacer></v-spacer>
     </v-row>
     <v-row>
-      <!--메뉴들 정보-->
-      <v-card class="mx-auto" flat>
-        <v-tabs
-          v-model="tab"
-          background-color="yellow darken-2"
-          show-arrows
-          center-active
-        >
-          <v-tabs-slider color="yellow darken-5"></v-tabs-slider>
+      <v-col>
+        <!--메뉴들 정보-->
+        <v-card class="mx-auto" flat>
+          <v-tabs v-model="tab" background-color="yellow darken-2">
+            <v-tabs-slider color="yellow darken-5"></v-tabs-slider>
 
-          <v-tab v-for="category in categories" :key="category">
-            {{ category }}
-          </v-tab>
-        </v-tabs>
+            <v-tab v-for="category in categories" :key="category">
+              {{ category }}
+            </v-tab>
+          </v-tabs>
 
-        <v-tabs-items v-model="tab" class="mx-auto">
-          <v-tab-item v-for="ele in menus" :key="ele.product_id">
-            <v-card flat>
-              <v-container>
-                <v-data-iterator
-                  :items="
-                    menus.filter((it) =>
-                      it.product_category.includes(categories[tab])
-                    )
-                  "
-                  :search="search"
-                  hide-default-footer
-                >
-                  <template v-slot:default="props">
-                    <v-row>
-                      <v-col
-                        v-for="item in props.items"
-                        :key="item.product_id"
-                        cols="12"
-                        sm="6"
-                        md="4"
-                        lg="3"
-                      >
-                        <v-card @click.native="orderProduct(item.product_id)">
-                          <v-img
-                            height="200"
-                            width="auto"
-                            src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
-                          ></v-img>
-                          <v-card-title
-                            >{{ item.product_name }} <v-spacer></v-spacer
-                            >{{ item.product_price }}
-                          </v-card-title>
-                        </v-card>
-                      </v-col>
-                    </v-row>
-                  </template>
-                </v-data-iterator>
-              </v-container>
-            </v-card>
-          </v-tab-item>
-        </v-tabs-items>
-      </v-card>
+          <v-tabs-items v-model="tab">
+            <v-tab-item v-for="ele in menus" :key="ele.product_id">
+              <v-card flat>
+                <v-container>
+                  <v-data-iterator
+                    :items="
+                      menus.filter((it) =>
+                        it.product_category.includes(categories[tab])
+                      )
+                    "
+                    :search="search"
+                    hide-default-footer
+                  >
+                    <template v-slot:default="props">
+                      <v-row>
+                        <v-col
+                          v-for="item in props.items"
+                          :key="item.product_id"
+                          cols="12"
+                          sm="6"
+                          md="4"
+                          lg="3"
+                        >
+                          <v-card @click.native="orderProduct(item.product_id)">
+                            <v-img
+                              height="200"
+                              width="auto"
+                              src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
+                            ></v-img>
+                            <v-card-title
+                              >{{ item.product_name }} <v-spacer></v-spacer
+                              >{{ item.product_price }}
+                            </v-card-title>
+                          </v-card>
+                        </v-col>
+                      </v-row>
+                    </template>
+                  </v-data-iterator>
+                </v-container>
+              </v-card>
+            </v-tab-item>
+          </v-tabs-items>
+        </v-card>
+      </v-col>
     </v-row>
     <!--메뉴 누르면 뜨는 다이얼로그-->
     <v-dialog v-if="dialog" v-model="dialog" max-width="500" max-height="700">
@@ -206,6 +257,7 @@ export default {
   data() {
     return {
       storeInfo: {},
+      storeCategory: [],
       errorMsg: "",
       dialog: false,
       snackbar: false,
@@ -213,7 +265,7 @@ export default {
       productId: "",
       tab: null,
       menus: {},
-      categories: {},
+      categories: [],
       dayOfTheWeek: ["월", "화", "수", "목", "금", "토", "일"],
       search: "",
       inset: false,
@@ -223,6 +275,18 @@ export default {
     try {
       const res = await axios.get("/store/" + this.$route.query.id);
       this.storeInfo = res.data;
+      let holiday = [];
+      const parsedholiday = this.storeInfo.holiday.split("").map(Number);
+      parsedholiday.forEach((element, idx) => {
+        if (element === 1) {
+          holiday.push(this.dayOfTheWeek[idx]);
+        }
+      });
+      this.storeInfo.holiday = holiday;
+      const storeCategoryResponse = await axios.get(
+        "/store/category/" + this.$route.query.id
+      );
+      this.storeCategory = storeCategoryResponse.data;
     } catch (error) {
       this.errorMsg = error.response.data.message;
     }
