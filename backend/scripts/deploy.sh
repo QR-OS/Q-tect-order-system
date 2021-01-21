f3a8b1f74b4df4b4b68b2c3e7e2d580f328da391
small-j <44703262+small-j@users.noreply.github.com>
2021-01-21 11:38:03 +0900

    Update deploy.sh

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -14,12 +14,12 @@ then
         echo ">실행 중인 jar 없음." >> /home/ubuntu/log/b_deploy.log
 else
         echo ">sudo kill -15 $CURRENT_PID" >> /home/ubuntu/log/b_deploy.log
-        kill -15 "$CURRENT_PID" >> /home/ubuntu/log/b_deploy.log 2>&1
-        sleep 5
+        sudo kill -15 "$CURRENT_PID" >> /home/ubuntu/log/b_deploy.log 2>&1
+        sudo sleep 5
 fi
 
 
 echo ">$JAR_PATH 배포"
-nohup java -jar "$JAR_PATH" >> /home/ubuntu/log/b_deploy.log 2>&1 &
+sudo nohup java -jar "$JAR_PATH" >> /home/ubuntu/log/b_deploy.log 2>&1 &
 
 echo "[$(date)] server deploy" >> /home/ubuntu/log/b_deploy.log
